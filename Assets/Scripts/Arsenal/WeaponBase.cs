6cc8269517cb02a78c81be2b2f48c482b423a243
radek.dev <radek.dev.jasinski@gmail.com>
2024-07-22 15:02:04 +0200

    demo2

--- Assets/Scripts/Arsenal/WeaponBase.cs
+++ Assets/Scripts/Arsenal/WeaponBase.cs
@@ -2,28 +2,29 @@ using UnityEngine;
 
 public abstract class WeaponBase : MonoBehaviour, IWeapon
 {
+    //pola u¿ywane przez ka¿d¹ broñ
     public bool IsActive { get; set; }  
     public int Level { get; set; }
     public float Cooldown { get; set; }
     protected float cooldownTimer;
 
-   
+   //metoda aktywuj¹ca umiejêtnoœæ
     public void Activate()
     {
         if (!IsActive)
         {
-            //Execute();
             cooldownTimer = Cooldown;
             IsActive = true;
         }
     }
-
+    //mechanizm zegara w funkcji Update
     protected virtual void Update()
     {
         if (cooldownTimer <= 0)
         {
             if (IsActive)
             {
+                //je¿eli czas min¹³ i umiejêtnoœæ jest aktywna - wykonaj akcjê
                 Execute();
                 cooldownTimer = Cooldown;
             }
@@ -33,6 +34,6 @@ public abstract class WeaponBase : MonoBehaviour, IWeapon
             cooldownTimer -= Time.deltaTime;
         }
     }
-
+    //abstrakcyjna metoda wykonania umiejêtnoœci - ka¿da broñ musi mieæ tak¹ funkcjê
     protected abstract void Execute();
 }
